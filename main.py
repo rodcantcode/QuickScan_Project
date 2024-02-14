1f8296f9c9e5d00d0e017aa2654abeb66b105821
Rod <codingwithrod@gmail.com>
2024-02-14 00:19:12 -0500

    Created bounded boxes around all readable text

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 #Take in Image input
 import pytesseract
-#import easyocr
+import re
 import cv2
 import numpy as np
-#from PIL import Image
-
+from pytesseract import Output
 
 img = cv2.imread('data/workschedule1.jpeg')
-cv2.imshow('Window1', img)
+d = pytesseract.image_to_data(img, output_type=Output.DICT)
+keys = list(d.keys())
+print(d.keys())
+
+#date_pattern = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
+
+
+n_boxes = len(d['text'])
+for i in range(n_boxes):
+    if int(d['conf'][i]) > 10:
+        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
 #img_file = "data/workschedule2.png"
 #img_file = "data/workschedule3.png"
-
-ocr_results = pytesseract.image_to_string(img)
-print(ocr_results)
+cv2.imshow('img', img)
+cv2.waitKey(0)
+#ocr_results = pytesseract.image_to_string(img)
+#print(ocr_results)
